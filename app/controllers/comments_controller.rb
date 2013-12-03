bba6845e127a8d05baffedfb171f513f65455b37
Edward Fernando <edward.fer@gmail.com>
2013-12-03 18:55:02 +0700

    Finish implementing code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,14 @@ class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
 		@comments = @post.comments.create(params[:comment].permit(:commenter, :body))
-		redirect_to_path(@post)
+		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
 	end
 
 end
