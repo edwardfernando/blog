92e4ff87e6e100e1cd2c7fc4f3f75bc2b3f709e5
Edward Fernando <edward.fer@gmail.com>
2013-12-11 15:48:01 +0700

    Change table structure

--- db/migrate/20131206163932_create_testimonials.rb
+++ db/migrate/20131206163932_create_testimonials.rb
@@ -1,14 +1,9 @@
 class CreateTestimonials < ActiveRecord::Migration
   def change
     create_table :testimonials do |t|
-      t.string :name
       t.text :comment
-      t.string :sex
-      t.string :location
-      t.integer :age
-      t.boolean :approved
-
       t.references :website, index: true
+      t.references :user, index: true
 
       t.timestamps
     end
