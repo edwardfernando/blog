5535974674999589eb4e4b2dd015a4754073a3a8
Edward Fernando <edward.fer@gmail.com>
2013-12-13 20:18:05 +0700

    Change mailer config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,5 @@ Blog::Application.configure do
 
   config.eager_load = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://calm-brushlands-5895.herokuapp.com/' }
 end
