c2c69ebd32e7de6ec1690e7b54e6299976f243ef
Edward Fernando <edward.fer@gmail.com>
2013-12-13 23:31:03 +0700

    delte omniauth config to prevent double facebook requests

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '653128454709649', '8d2781918919f88e36a4461705011eb7', :scope => 'email', :display => 'page'
-end
