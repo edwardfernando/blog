67f8b2e82a97de799dd167eedde8d0046731bf1d
Edward Fernando <edward.fer@gmail.com>
2013-12-13 18:51:05 +0700

    Delete user resource (revert back)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210113845) do
+ActiveRecord::Schema.define(version: 20131213104401) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -52,7 +52,20 @@ ActiveRecord::Schema.define(version: 20131210113845) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "websites", force: true do |t|
     t.string   "url"
