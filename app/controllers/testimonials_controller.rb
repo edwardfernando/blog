4a687d7b51a56fd3ea1ce796f8d561d49e45e141
Edward Fernando <edward.fer@gmail.com>
2013-12-26 17:52:20 +0700

    Fix testimonial controller

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -1,10 +1,9 @@
 class TestimonialsController < ApplicationController
 
 	def create
-		@website = Website.find(params[:website_id])
-		@testimonial = @website.testimonials.create(params[:testimonial].permit(:comment))
-		@testimonial.user_id = User.find(current_user.id)
-		redirect_to website_path(@website.thread_id)
+		website = Website.find(params[:website_id])
+		testimonial = website.testimonials.create(comment:params[:testimonial][:comment], user:current_user)	
+		redirect_to website_path(website.thread_id)
 	end
 
 end
