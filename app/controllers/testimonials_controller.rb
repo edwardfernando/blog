efdb52bf2cf2ae18ecadfb000eaeb2a09382af61
Edward Fernando <edward.fer@gmail.com>
2013-12-27 23:58:10 +0700

    Finish rich text area editor

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -2,7 +2,7 @@ class TestimonialsController < ApplicationController
 
 	def create
 		website = Website.find(params[:website_id])
-		testimonial = website.testimonials.create(comment:params[:testimonial][:comment], user:current_user)	
+		testimonial = website.testimonials.create(comment:params[:rich_textarea_content], user:current_user)	
 		redirect_to website_path(website.thread_id)
 	end
 
