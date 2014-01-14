f297a2bc7c9e035860421c313ad867e8221fba7b
Edward Fernando <edward.fer@gmail.com>
2014-01-14 21:13:45 +0700

    add index method and change #profile_complete misspelled & fix validation logic

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,24 @@
 class ProfilesController < ApplicationController
+	
+	def index
+		@user = current_user
+	end
+
 	def profile_init
 		@user = User.find(params[:id])
 	end
 
-	def profile_compelete
-		user = User.find(params[:id])
-		user.name = params[:name]
-		user.email = params[:email]
+	def profile_complete
+		@user = User.find(params[:id])
 
-		 user.save!
+		@user.name = params[:name]
+		@user.email = params[:email]
 
-		sign_in_and_redirect user, :event => :authentication
+		if @user.save
+			sign_in(@user)
+			redirect_to profiles_path
+		else
+			render 'profile_init'
+		end
 	end
 end
