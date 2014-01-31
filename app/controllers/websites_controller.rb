c20634ff2de1e798ee03af0786bafafeed02574f
Edward Fernando <edward.fer@gmail.com>
2014-01-31 21:42:52 +0700

    delete unused code

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -14,23 +14,6 @@ class WebsitesController < ApplicationController
 		@website = Website.new
 	end
 
-	# def new
-	# 	url = params[:website][:url]
-	# 	doc = Nokogiri::HTML(open(url))
-
-	# 	@website = Website.new
-	# 	@website.url = url
-	# 	@website.title = doc.css("h2.entry-title").text
-	# 	@website.content = doc.css("div.entry")[0].to_s
-	# end
-
-	# def create
-	# 	@website = Website.new(params[:website].permit(:url, :title, :content))
-	# 	@website.user = User.find(current_user.id)
-	# 	@website.save
-	# 	redirect_to @website
-	# end
-
 	def show
 		@website = Website.where(:thread_id => params[:id]).first
 		@testimonials = Testimonial.where(:website => @website).order(created_at: :desc)
