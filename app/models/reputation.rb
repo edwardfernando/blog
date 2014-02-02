d2d0959f2abe5a19cbcd5dc1b5958c49e5efc57c
Edward Fernando <edward.fer@gmail.com>
2014-02-02 23:01:14 +0700

    Remove dedicated reputation table

--- app/models/reputation.rb
+++
@@ -1,3 +0,0 @@
-class Reputation < ActiveRecord::Base
-	belongs_to :testimonial
-end
--- db/migrate/20140202143231_add_additional_information_to_reputation.rb
+++
@@ -1,8 +0,0 @@
-class AddAdditionalInformationToReputation < ActiveRecord::Migration
-  def change
-  	add_column :reputations, :accurate_description, :integer
-  	add_column :reputations, :communication, :integer
-  	add_column :reputations, :shipping_speed, :integer
-  	add_column :reputations, :shipping_cost, :integer
-  end
-end
